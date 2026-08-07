9abd449724ad7856e3cfe8f425424e9f0e637c88
Narendra singh Dhakad <142916613+Narendrasinghdhakad2020@users.noreply.github.com>
2026-08-07 15:04:20 +0530

    Update security filter chain configuration deleted http

--- src/main/java/com/chatApp/ChatApplication/config/SecurityConfig.java
+++ src/main/java/com/chatApp/ChatApplication/config/SecurityConfig.java
@@ -36,7 +36,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
+        
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(auth -> auth
